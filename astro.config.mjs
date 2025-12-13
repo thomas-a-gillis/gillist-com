7dd1795dded28a48f3efcdda7849a054e5d2af5b
Thomas A. Gillis <tom@gillist.com>
2025-12-13 13:54:24 -0500

    Configure site URL for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro';
 
 export default defineConfig({
   site: 'https://gillist.com',
-  output: 'static'
+  base: '/',
 });
